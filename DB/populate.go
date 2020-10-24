fa10027233fe7d8eca9fae5d21332dd4df930b70
nidhish1 <eyesgotshow@gmail.com>
2020-10-25 03:26:21 +0530

    db populated and constrainst added

--- DB/populate.go
+++ DB/populate.go
@@ -24,9 +24,17 @@ func dsn(dbName string) string {
 }
 
 type album struct {
-	UserID int `json:"userId"`
-	ID     int `json:"id"`
-	Title  int `json:"title"`
+	UserID int    `json:"userId"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+}
+
+type photo struct {
+	AlbumID   int    `json:"albumId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	URL       string `json:"url"`
+	Thumbnail string `json:"thumbnailUrl"`
 }
 
 func main() {
@@ -45,6 +53,7 @@ func main() {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
+	defer response.Body.Close()
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
@@ -53,6 +62,54 @@ func main() {
 
 	var albums []album
 	json.Unmarshal(responseData, &albums)
-	fmt.Printf("Birds : %+v", albums)
+
+	// for _, al := range albums {
+
+	// 	stmt, _ := db.Prepare("insert into album(id, userId, title) values (?, ?, ?)")
+	// 	_, _ = stmt.Exec(al.ID, al.UserID, al.Title)
+
+	// }
+
+	// for _, al := range albums {
+	// 	url := "https://jsonplaceholder.typicode.com/photos?albumId=" + fmt.Sprintf("%d", al.ID)
+	// 	fmt.Println(url)
+	// 	response, err := http.Get(url)
+	// 	if err != nil {
+	// 		fmt.Print(err.Error())
+	// 		os.Exit(1)
+	// 	}
+	// 	defer response.Body.Close()
+	// 	var photos []photo
+	// 	json.Unmarshal(responseData, &photos)
+	// 	fmt.Println(photos)
+
+	// }
+
+	for i, _ := range albums {
+		response, err = http.Get("https://jsonplaceholder.typicode.com/photos?albumId=" + fmt.Sprintf("%d", i+1))
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		defer response.Body.Close()
+
+		responseData, err = ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var photos []photo
+		json.Unmarshal(responseData, &photos)
+		fmt.Println(photos)
+
+		for _, ph := range photos {
+
+			stmt, _ := db.Prepare("insert into photo(id, albumId, title,url,thumbnailUrl) values (?, ?, ?,?,?)")
+			_, _ = stmt.Exec(ph.ID, ph.AlbumID, ph.Title, ph.URL, ph.Thumbnail)
+			defer stmt.Close()
+
+		}
+
+	}
 
 }
